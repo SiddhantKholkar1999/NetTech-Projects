ce3fff33019978737378c980cf1464c88c089de8
SiddhantKholkar1999 <siddhantkholkar1999@gmail.com>
2025-07-11 14:28:00 +0530

    promise concept 10.07.2025

--- Practice Feb 2025/JAVASCRIPT - 25.05.2025/javascript/Promise.js
+++ Practice Feb 2025/JAVASCRIPT - 25.05.2025/javascript/Promise.js
@@ -21,3 +21,38 @@ ice_cream
     .catch(() => {
         console.log('call mommy');
     });
+
+//! What if we get delay to get the response
+console.log('A');
+
+let ice_cream_1 = new Promise((res, rej) => {
+    let got_ice_cream_1;
+    console.log('B');
+    /*
+    $ setTimeout(()=>{},delay)
+    */
+    
+    setTimeout(() => {
+        got_ice_cream_1 = true;
+        if (got_ice_cream_1) {
+            res(got_ice_cream_1);
+        } else {
+            rej(got_ice_cream_1);
+        }
+    }, 3000);
+
+    console.log('C');
+    
+}); // here I created new promises
+
+// we started the promises
+console.log('D');
+
+ice_cream_1
+    .then(() => {
+        console.log('eat ice cream');
+    })
+    .catch(() => {
+        console.log('call mommy');
+    });
+
