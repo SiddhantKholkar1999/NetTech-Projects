01b446992b74fa7097d150be6d47559cbd8c5a59
SiddhantKholkar1999 <siddhantkholkar1999@gmail.com>
2025-07-22 16:18:29 +0530

    Fetch & CRUD 3

---
+++ Practice Feb 2025/JAVASCRIPT - 25.05.2025/javascript/fetch1_CAT_API.js
@@ -0,0 +1,3 @@
+const API_KEY = "live_ZwbEVGZHYamzmdGYpOz6GaxbEt4ymfop4iFuvxyGUJWFZtqvOpJsusa36sgngCWf";
+
+const api = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${API_KEY}`;
---
+++ Practice Feb 2025/JAVASCRIPT - 25.05.2025/javascript/fetch1_DOG_API.js
@@ -0,0 +1,3 @@
+const API_KEY = "live_bbRYLR1QG1FAv1AuqXXtVGIi8V6wfcmoP39A7bh4uKQ6JK80N59NZqU0Nl9k4W3J";
+
+const api = `https://api.thedogapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=${API_KEY}`;
