83ec43948d9873c78a577ea40d929f4f367a6f53
SiddhantKholkar1999 <siddhantkholkar1999@gmail.com>
2025-09-18 15:25:33 +0530

    React - TODO

--- REACT_01-09-2025/TODO_APP/src/Components/AddTodo.jsx
+++ REACT_01-09-2025/TODO_APP/src/Components/AddTodo.jsx
@@ -5,8 +5,10 @@ export const AddTodo = () => {
 
     const [data, setData] = React.useState([]);
     const [text, setText] = React.useState('');
+    const inputData = React.useRef(null);
 
-    const handleFunc = () => {
+    //By UseState
+    const handleFuncByUseState = () => {
         if (text.trim() === "") return;
 
         let todo = {
@@ -14,17 +16,34 @@ export const AddTodo = () => {
             text,
             isEdit: false,
             isCheck: false,
-        }
+        };
 
         setData([...data, todo]);
+    }
+
+    //By UseRef
+    const handleFuncByUseRef = () => {
+        if (inputData.current.value.trim() === "") return;
+
+        let todo = {
+            id: Date.now(),
+            todo: inputData.current.value,
+            isEdit: false,
+            isComplete: false,
+        };
+
+        sessionStorage.setItem("todos", JSON.stringify(todo));
     }
 
     return (
         <>
             {/* <div>AddTodo</div> */}
-            <h1>TODO </h1>
+            <h1>TODO By UseState</h1>
             <input onChange={(e) => setText(e.target.value)} type='text' placeholder='Enter the todo task.....'/>
-            <input onClick={handleFunc} type='button' value="add todo" />
+            <input onClick={handleFuncByUseState} type='button' value="add todo" />
+            <h1>TODO By UseRef</h1>
+            <input type='text' placeholder='enter the todo....' ref={inputData} />
+            <button onClick={handleFuncByUseRef}>add</button>
             <ListTodo props={data}/>
         </>
     )
