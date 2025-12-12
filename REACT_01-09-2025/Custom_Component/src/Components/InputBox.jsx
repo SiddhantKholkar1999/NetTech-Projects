0d9950ae3856ce21fa6c6f68b7390f67f98e3887
SiddhantKholkar1999 <siddhantkholkar1999@gmail.com>
2025-12-12 15:27:52 +0530

    React - Custom_Component

--- REACT_01-09-2025/Custom_Component/src/Components/InputBox.jsx
+++ REACT_01-09-2025/Custom_Component/src/Components/InputBox.jsx
@@ -1,19 +1,38 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import PropTypes from "prop-types";
 import { useState } from "react";
 import { PinItems } from "./PinItems";
+import { useRef } from "react";
 
 export const InputBox = ({ label, length, perBox }) => {
   //   console.log("🚀 ~ label:", typeof label, typeof length);
   const [data, setData] = useState(new Array(length).fill(""));
+  const element = useRef(new Array(length).fill(0));
 
-  const handleFunc = () => {};
+  const handleAddFunc = (values, index) => {
+    const val = [...data];
+    val[index] = values;
+    setData(val);
+    if (index < length - 1) {
+      element.current[index + 1]?.focus();
+    }
+  };
+  console.log("🚀 ~ data:", data);
   return (
     <>
       {/* <h1>{label}</h1> */}
       {data &&
         data.map((el, index) => {
-          return <PinItems max={perBox} key={index} onChange={handleFunc} />;
+          return (
+            <PinItems
+              ref={(n) => (element.current[index] = n)}
+              max={perBox}
+              key={index}
+              //   onChange={handleFunc}
+              onChange={(values) => handleAddFunc(values, index)}
+            />
+          );
         })}
     </>
   );
--- REACT_01-09-2025/Custom_Component/src/Components/PinItems.jsx
+++ REACT_01-09-2025/Custom_Component/src/Components/PinItems.jsx
@@ -10,15 +10,57 @@ const style = {
   textAlign: "center",
 };
 
-export const PinItems = ({ max }) => {
+export const PinItems = React.forwardRef(({ max, onChange }, ref) => {
+  console.log("🚀 ~ onChange:", onChange);
+  const handleKeyUp = (e) => {
+    console.log("🚀 ~ e:", e);
+    console.log("🚀 ~ e.keyCode:", e.keyCode);
+    async function pasteTextFromClipboard() {
+      try {
+        const clipboardText = await navigator.clipboard.readText();
+        console.log("🚀 ~ clipboardText:", clipboardText);
+      } catch (error) {
+        console.error("Failed to paste", error);
+      }
+    }
+    switch (e.keyCode) {
+      case 9:
+        //for tab button default behaviour
+        e.preventDefault();
+        break;
+
+      case 8:
+        //for backspace button default behaviour
+        e.preventDefault();
+        break;
+
+      default:
+        onChange(e.target.value);
+        pasteTextFromClipboard();
+    }
+  };
   return (
     <>
-      {/* <h1></h1> */}
-      <input type="text" style={style} maxLength={max} />
+      <input
+        ref={ref}
+        type="text"
+        style={style}
+        maxLength={max}
+        onKeyUp={handleKeyUp}
+      />
     </>
   );
-};
+});
+// export const PinItems = ({ max }) => {
+//   return (
+//     <>
+//       {/* <h1></h1> */}
+//       <input type="text" style={style} maxLength={max} />
+//     </>
+//   );
+// };
 
 PinItems.propTypes = {
   max: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
